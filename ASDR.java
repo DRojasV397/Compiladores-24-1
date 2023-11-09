f5a5f958904b2094980d407acba652e2c1e29b24
DRojasV397 <diegorv3971@gmail.com>
2023-11-08 21:14:49 -0600

    Update ASDR.java

--- ASDR.java
+++ ASDR.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.print.DocFlavor.STRING;
 
 public class ASDR implements Parser{
     private int i = 0;
     private boolean hayErrores = false;
     private Token preanalisis;
     private final List<Token> tokens;
-
+    private List<TipoToken> primeroEXPRESSION; //Se declara una lista en la que se incluiran los tokens que forman el conjunto primero de EXPRESSION
 
     public ASDR(List<Token> tokens){
         this.tokens = tokens;
         preanalisis = this.tokens.get(i);
+
+        //Se agregan los elementos del conjunto primero de EXPRESSION
+        primeroEXPRESSION.add(TipoToken.BANG);
+        primeroEXPRESSION.add(TipoToken.TRUE);
+        primeroEXPRESSION.add(TipoToken.FALSE);
+        primeroEXPRESSION.add(TipoToken.NULL);
+        primeroEXPRESSION.add(TipoToken.NUMBER);
+        primeroEXPRESSION.add(TipoToken.STRING);
+        primeroEXPRESSION.add(TipoToken.IDENTIFIER);
+        primeroEXPRESSION.add(TipoToken.LEFT_PAREN);
     }
 
     @Override
@@ -26,10 +39,257 @@ public class ASDR implements Parser{
 
     // PROGRAM -> DECLARATION
     private void PROGRAM(){
-        DECLARATION();
+            DECLARATION();
     }
+    
+    /************************************************************************
+     *                            Declaraciones
+    ************************************************************************/
 
     private void DECLARATION(){
-        return;
+        if(hayErrores) //Termina el 
+            return;
+
+        /*
+         * Dentro de cada if se compara con los elementos del conjunto primero 
+         * de cada uno de los no terminales
+         */
+
+        // DECLARATION -> FUN_DECL DECLARATION
+        else if(preanalisis.tipo == TipoToken.FUN){
+            FUN_DECL();
+            DECLARATION();
+        }
+        // DECLARATION -> VAR_DECL DECLARATION
+        else if (preanalisis.tipo == TipoToken.VAR){
+            VAR_DECL();
+            DECLARATION();
+        }
+        // DECLARATION -> STATEMENT DECLARATION
+        else if (preanalisis.tipo == TipoToken.EQUAL){
+            STATEMENT();
+            DECLARATION();
+        }
+
+        /*
+        * La producción DECLARATION -> Ɛ no se incluye en codigo, debido a esto, 
+        * si el tipo de preanalisis no coincide con ninguno de los tipos de los if
+        * No se conciderará como error.
+        */
+
     }
+    
+    // FUN_DECL -> fun FUNCTION
+    private void FUN_DECL(){
+        if(hayErrores)
+            return;
+        
+        /*
+         * En esta función, a diferencia de la funcion anterior, primero 
+         * se compara que el elemento del preanalisis sea FUN, ya que si 
+         * no coincide se trata de un error. Las producciones de este 
+         * no terminal no contemplan Ɛ, por lo que si se recibe un terminal
+         * distinto será un error.
+         */
+        
+        if(preanalisis.tipo == TipoToken.FUN){
+            match(TipoToken.FUN);
+            FUNCTION();
+        }
+        else{
+            hayErrores = true;
+            System.out.println("Se esperaba una FUNCION");
+        }
+    }
+
+    // VAR_DECL -> var id VAR_INIT ;
+    private void VAR_DECL(){
+        if(hayErrores)
+            return;
+        
+        /*
+         * En esta función primero se compara que el elemento del preanalisis 
+         * sea VAR, ya que si no coincide se trata de un error. Las producciones 
+         * de este no terminal no contemplan Ɛ, por lo que si se recibe un terminal
+         * distinto será un error; es por ello que EL PRIMER IF-ELSE ES NECESARIO.
+         * 
+         * El if-else dentro del primero se usa para detallar el error. No es 
+         * necesario seguir esta estructura, en seguida se muestra otra forma 
+         * que se puede usar para detallar el error.
+         */
+        
+        if(preanalisis.tipo == TipoToken.VAR){
+            match(TipoToken.VAR);
+            if(preanalisis.tipo == TipoToken.IDENTIFIER){
+                match(TipoToken.IDENTIFIER);
+                VAR_INIT();
+
+                /*
+                 * Esta es otra forma de manejar los errores.
+                 * Despues de usar la función match() se verifica si hay errores, si
+                 * los hay se muestra el mensaje de error y se usa "return".
+                 * Usar "return" es necesario para evitar que  
+                 */
+                match(TipoToken.SEMICOLON);
+                if(hayErrores){
+                    System.out.println("Se esperaba ';'");
+                    return;
+                }
+            }
+            else{
+                hayErrores = true;
+                System.out.println("Se esperaba un IDENTIFICADOR luego de VAR.");
+            }
+        }
+        else{
+            hayErrores = true;
+            System.out.println("Se esperaba una VARIABLE");
+        }
+    }
+
+    // VAR_INIT -> = EXPRESSION | Ɛ
+    private void VAR_INIT(){
+        if(hayErrores)
+            return;
+
+        if(preanalisis.tipo == TipoToken.EQUAL){
+            match(TipoToken.EQUAL);
+            if (hayErrores) {
+                System.out.println("Se esperaba '='");
+                return;
+            }
+            EXPRESSION();
+        }
+
+        /*
+        * La producción DECLARATION -> Ɛ no se incluye en codigo, debido a esto, 
+        * si el tipo de preanalisis no coincide con ninguno de los tipos de los if
+        * No se conciderará como error.
+        */
+        
+    }
+    
+    /************************************************************************
+     *                              Sentencias
+    ************************************************************************/
+
+
+    /************************************************************************
+     *                              Expresiones
+    ************************************************************************/
+
+
+    /************************************************************************
+     *                                 Otras
+    ************************************************************************/
+
+    //FUNCTION -> id ( PARAMETERS_OPC ) BLOCK
+    private void FUNCTION(){
+        if(hayErrores)
+            return;
+
+        if(preanalisis.tipo == TipoToken.IDENTIFIER){
+            match(TipoToken.IDENTIFIER);
+            match(TipoToken.LEFT_PAREN);
+            if(hayErrores){
+                System.out.println("Se esperaba '(''");
+                return;
+            }
+            PARAMETERS_OPC();
+            match(TipoToken.RIGHT_PAREN);
+            if (hayErrores) {
+                System.out.println("Se esperaba ')''");
+                return;
+            }
+            BLOCK();
+        }
+        else{
+            hayErrores = true;
+            System.out.println("Se esperaba un IDENTIFICADOR");
+        }
+    }
+
+    // PARAMETERS_OPC -> PARAMETERS | Ɛ
+    private void PARAMETERS_OPC(){
+        if(hayErrores)
+            return;
+
+        if(preanalisis.tipo == TipoToken.IDENTIFIER)
+            PARAMETERS();
+    }
+
+    // PARAMETERS -> id PARAMETERS_2
+    private void PARAMETERS(){
+        if(hayErrores)
+            return;
+
+        if(preanalisis.tipo == TipoToken.IDENTIFIER){
+            match(TipoToken.IDENTIFIER);
+            PARAMETERS_2();
+        }
+        else{
+            hayErrores = true;
+            System.out.println("Se esperaba un IDENTIFICADOR");
+        }
+    }
+
+    //PARAMETERS_2 -> , id PARAMETERS_2 | Ɛ
+    private void PARAMETERS_2(){
+        if(hayErrores)
+            return;
+
+        if(preanalisis.tipo == TipoToken.COMMA){
+            match(TipoToken.COMMA);
+            match(TipoToken.IDENTIFIER);
+            if (hayErrores) {
+                System.out.println("Se esperaba un IDENTIFICADOR");
+                return;
+            }
+            PARAMETERS_2();
+        }
+    }
+
+    // ARGUMENTS_OPC -> EXPRESSION ARGUMENTS | Ɛ
+    private void ARGUMENTS_OPC(){
+        if(hayErrores)
+            return;
+        
+        /*
+         * Se verifica si el tipo de preanalisis pertenece al conjunto primero de EXPRESSION, 
+         * de lo contrario se tomara como la producción Ɛ.
+         * Esto se podria hacer comparando de forma directa dentro del if el tipo de preanalisis 
+         * con los elementos del conjunto primero de EXPRESSION pero de esta forma el codigo se 
+         * vuelve mas legible.
+         */
+        if(primeroEXPRESSION.contains(preanalisis.tipo)){
+            EXPRESSION();
+            ARGUMENTS();
+        }
+    }
+
+    // ARGUMENTS -> , EXPRESSION ARGUMENTS | Ɛ
+    private void ARGUMENTS(){
+        if(hayErrores)
+            return;
+        
+        if(preanalisis.tipo == TipoToken.COMMA){
+            match(TipoToken.COMMA);
+            EXPRESSION();
+            ARGUMENTS();
+        }
+    }
+
+
+    /************************************************************************
+     *                                  Match
+    ************************************************************************/
+    private void match(TipoToken tt){
+            if(preanalisis.tipo == tt){
+                i++;
+                preanalisis = tokens.get(i);
+            }
+            else{
+                hayErrores = true;
+            }
+        }
 }
